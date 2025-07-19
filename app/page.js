5c8f00beec94e9d730081ca69ce72adbbbba2cb7
ASayyam <139552489+ASayyam@users.noreply.github.com>
2025-07-19 16:34:45 +0500

    Correction

--- app/page.js
+++ app/page.js
@@ -1,16 +1,17 @@
-
-        import Head from 'next/head';
+import Image from 'next/image';
+import Head from 'next/head';
 import styles from '../styles/CV.module.css';
+
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Asma Sayyam's CV</title>
+        <title>Asma Sayyam CV</title>
       </Head>
       <main className={styles.main}>
         {/* <h1 className={styles.title}>ASMA SAYYAM</h1> */}
-        <a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.herokuapp.com?font=Arial&size=35&duration=5004&pause=1000&color=2FF72D&random=false&width=435&lines=Hi+there%2C+I'm+Asma+Sayyam" alt="Typing SVG" /></a>
+        <a href="https://git.io/typing-svg"><Image src="https://readme-typing-svg.herokuapp.com?font=Arial&size=35&duration=5004&pause=1000&color=2FF72D&random=false&width=435&lines=Hi+there%2C+I'm+Asma+Sayyam" alt="Typing SVG" /></a>
         
         <p>Green Avenue, Islamabad, Pakistan</p>
         <p>Cell #:  0336-5582358 / 0332-5601171</p>
@@ -23,7 +24,7 @@ export default function Home() {
         
         <section className={styles.section}>
           <h2>PERSONAL INFORMATION:</h2>
-          <p>Father’s Name: Muhammad Hussain Tallat</p>
+          <p>Father Name: Muhammad Hussain Tallat</p>
           <p>C.N.I.C #: 61101-9096055-2</p>
           <p>Religion: Islam</p>
         
@@ -31,7 +32,7 @@ export default function Home() {
         
         <section className={styles.section}>
           {/* <h2>ACADEMIC QUALIFICATION:</h2> */}
-          <a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.herokuapp.com?font=Arial&size=30&duration=5004&pause=1000&color=2FF72D&random=false&width=435&lines=ACADEMIC QUALIFICATION" alt="Typing SVG" /></a>
+          <a href="https://git.io/typing-svg"><Image src="https://readme-typing-svg.herokuapp.com?font=Arial&size=30&duration=5004&pause=1000&color=2FF72D&random=false&width=435&lines=ACADEMIC QUALIFICATION" alt="Typing SVG" /></a>
           <table className={styles.table}>
             <thead>
               <tr>
@@ -82,39 +83,39 @@ export default function Home() {
         
         <section className={styles.section}>
           {/* <h2>WORKSHOPS ATTENDED:</h2> */}
-          <a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.herokuapp.com?font=Arial&size=30&duration=5004&pause=1000&color=2FF72D&random=false&width=435&lines=WORKSHOPS ATTENDED" alt="Typing SVG" /></a>
+          <a href="https://git.io/typing-svg"><Image src="https://readme-typing-svg.herokuapp.com?font=Arial&size=30&duration=5004&pause=1000&color=2FF72D&random=false&width=435&lines=WORKSHOPS ATTENDED" alt="Typing SVG" /></a>
           <ul>
-            <li>Attended the workshop on “Nelson English & Urdu ka Guldasta” by Oxford University Press on 10th Jan. 2017.</li>
-            <li>“Certificate of Participation” in training workshop on “Educational Leadership and Management” October 2017, organized by AFAQ and NUML Islamabad.</li>
-            <li>Certificate of Successful Participation in Learn how to teach English, 3-day Teachers’ Training Workshop on “Teaching of English in Schools” on 14-16, 2017 organized by Association For Academic Quality (AFAQ).</li>
-            <li>Certificate for successful completion of “Beyond ABC: Synthetic Phonics Workshop” organized by LTH( London Tesol House), Dec. 2017.</li>
-            <li>Workshop on “Scholastic Active English” held by Paramount at IIUI campus Islamabad on 17th Feb. 2018.</li>
-            <li>Certificate of Participation in a workshop on “ Marshall Cavendish English Heads up”, held at Islamabad. 24th Jan. 2019</li>
+            <li>Attended the workshop on Nelson English & Urdu ka Guldasta by Oxford University Press on 10th Jan. 2017.</li>
+            <li>Certificate of Participation in training workshop on Educational Leadership and Management October 2017, organized by AFAQ and NUML Islamabad.</li>
+            <li>Certificate of Successful Participation in Learn how to teach English, 3-day Teachers Training Workshop on Teaching of English in Schools on 14-16, 2017 organized by Association For Academic Quality (AFAQ).</li>
+            <li>Certificate for successful completion of Beyond ABC: Synthetic Phonics Workshop” organized by LTH( London Tesol House), Dec. 2017.</li>
+            <li>Workshop on Scholastic Active English held by Paramount at IIUI campus Islamabad on 17th Feb. 2018.</li>
+            <li>Certificate of Participation in a workshop on  Marshall Cavendish English Heads up, held at Islamabad. 24th Jan. 2019</li>
             <li>Certificate of participation in a workshop on Learning A-Z ( RAZ Plus), organized by Paramount, held at Ramada, 16th Feb. 2019.</li>
-            <li>Certificate of active participation in “ Using Stories & Visuals to Enhance Literacy Development” workshop, University of British Columbia. June 2019.</li>
-            <li>Certificate of active participation in “ School Leader as a Game Changer” workshop by Tiflee Education. November 2019.</li>
-            <li>Certificate of successful participation in one day online training workshop on “ 150 Teaching Methods” hold on 28th March 2021, conducted by YDI Training and Consultancy Mardan in Collaboration with OFAQ KPK.</li>
-            <li> Certificate of successful participation in one day online training workshop on “ The Successful Methods to Teach Reading” hold on 3rd April 2021, conducted by EDGE (Empowerment Development Growth Evolution) in collaboration with Evolution</li>
+            <li>Certificate of active participation in  Using Stories & Visuals to Enhance Literacy Development workshop, University of British Columbia. June 2019.</li>
+            <li>Certificate of active participation in  School Leader as a Game Changer workshop by Tiflee Education. November 2019.</li>
+            <li>Certificate of successful participation in one day online training workshop on  150 Teaching Methods” hold on 28th March 2021, conducted by YDI Training and Consultancy Mardan in Collaboration with OFAQ KPK.</li>
+            <li> Certificate of successful participation in one day online training workshop on  The Successful Methods to Teach Reading” hold on 3rd April 2021, conducted by EDGE (Empowerment Development Growth Evolution) in collaboration with Evolution</li>
           </ul>
         </section>
         
         <section className={styles.section}>
           {/* <h2>SHORT COURSES:</h2> */}
-          <a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.herokuapp.com?font=Arial&size=30&duration=5004&pause=1000&color=2FF72D&random=false&width=435&lines=SHORT COURSES" alt="Typing SVG" /></a>
+          <a href="https://git.io/typing-svg"><Image src="https://readme-typing-svg.herokuapp.com?font=Arial&size=30&duration=5004&pause=1000&color=2FF72D&random=false&width=435&lines=SHORT COURSES" alt="Typing SVG" /></a>
           <ul>
-            <li>“IT Skills Beginners Course”, STI (Sitara Teachers’ Institute) 2012.</li>
-            <li>“English language Course for Teachers” with A+ grade, Gosha-e-Msahal, 2013.</li>
-            <li>“Child Development, Teaching Course”, STI, 2012.</li>
-            <li>“Teaching For Understanding”, STI, 2013.</li>
-            <li>“IT Skills MS Word & MS Excel”, STI, 2012.</li>
+            <li>IT Skills Beginners Course, STI (Sitara Teachers Institute) 2012.</li>
+            <li>English language Course for Teachers with A+ grade, Gosha-e-Msahal, 2013.</li>
+            <li>Child Development, Teaching Course, STI, 2012.</li>
+            <li>Teaching For Understanding, STI, 2013.</li>
+            <li>IT Skills MS Word and MS Excel, STI, 2012.</li>
           </ul>
         </section>
 
         <section className={styles.section}>
           {/* <h2>ONGOING COURSE:</h2> */}
-          <a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.herokuapp.com?font=Arial&size=30&duration=5004&pause=1000&color=2FF72D&random=false&width=435&lines=ONGOING COURSE" alt="Typing SVG" /></a>
+          <a href="https://git.io/typing-svg"><Image src="https://readme-typing-svg.herokuapp.com?font=Arial&size=30&duration=5004&pause=1000&color=2FF72D&random=false&width=435&lines=ONGOING COURSE" alt="Typing SVG" /></a>
           <ul>
-            <li>"Certified Cloud Native Applied Generative AI Engineer" course from The Presidential Initiative for Artificial Intelligence and Computing (PIAIC), started in 2023.</li>
+            <li>Certified Cloud Native Applied Generative AI Engineer course from The Presidential Initiative for Artificial Intelligence and Computing (PIAIC), started in 2023.</li>
             In this course, I learned about:
             <li>Agentic AI</li>
             <li>Generative AI</li>
@@ -155,28 +156,28 @@ export default function Home() {
         
         <section className={styles.section}>
           {/* <h2>CERTIFICATES:</h2> */}
-          <a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.herokuapp.com?font=Arial&size=30&duration=5004&pause=1000&color=2FF72D&random=false&width=435&lines=CERTIFICATES" alt="Typing SVG" /></a>
+          <a href="https://git.io/typing-svg"><Image src="https://readme-typing-svg.herokuapp.com?font=Arial&size=30&duration=5004&pause=1000&color=2FF72D&random=false&width=435&lines=CERTIFICATES" alt="Typing SVG" /></a>
           <ul>
-            <li> “Certificate of Appreciation” from Sitara School and Teachers’ Institute for dedication & service for the session 2013-14.</li>
-            <li>“Certificate of Appreciation” from Sitara School and Teachers’ Institute for dedication & service for the session 2014-15.</li>
-            <li>“Certificate of Appreciation” from Sitara school and Teachers’ Institute for dedication & service for the session 2015-16.</li>
-            <li>“Certificate of Appreciation” from Sitara school and Teachers’ Institute for dedication & service for the session 2016-17.</li>
-            <li>“Certificate of Participation” in 1st National Graduate Conference on 15-16 March 2017 at Allama Iqbal Open University (AIOU).</li>
+            <li> “Certificate of Appreciation from Sitara School and Teachers Institute for dedication and service for the session 2013-14.</li>
+            <li>“Certificate of Appreciation from Sitara School and Teachers Institute for dedication and service for the session 2014-15.</li>
+            <li>“Certificate of Appreciation from Sitara school and Teachers Institute for dedication and service for the session 2015-16.</li>
+            <li>“Certificate of Appreciation from Sitara school and Teachers Institute for dedication and service for the session 2016-17.</li>
+            <li>“Certificate of Participation in 1st National Graduate Conference on 15-16 March 2017 at Allama Iqbal Open University (AIOU).</li>
             <li>“Certificate of Best Oral Presentation” in 1st Graduate Conference on 15-16 March 7 at Allama Iqbal Open University (AIOU).</li>
-            <li>“Certificate of Participation” in Private School Management Association (PSMA) Regd. 15th annual prize distribution and best teacher award ceremony On 9th November, 2017.</li>
-            <li>“Certificate of Participation” in worldwide project “Innovate Your Dreams” April 1 to March 31, 2019.</li>
-            <li>“Certificate of Appreciation” by Sitara School and Teachers’ Institute in recognition of outstanding contributions to the academic and spiritual growth and development of Sitara School students for the session 2017-2018.</li>
-            <li>“Certificate of Appreciation” by Sitara School and Teachers’ Institute in recognition of outstanding contributions to the academic and spiritual growth and development of Sitara School students for the session 2018-2019.</li>
-            <li> “Certificate of Appreciation” by Sitara School and Teachers’ Institute in recognition of outstanding contributions to the academic and spiritual growth and development of Sitara School students for the session 2019-2020.</li>
+            <li>“Certificate of Participation in Private School Management Association (PSMA) Regd. 15th annual prize distribution and best teacher award ceremony On 9th November, 2017.</li>
+            <li>“Certificate of Participation in worldwide project Innovate Your Dreams April 1 to March 31, 2019.</li>
+            <li>“Certificate of Appreciation by Sitara School and Teachers Institute in recognition of outstanding contributions to the academic and spiritual growth and development of Sitara School students for the session 2017-2018.</li>
+            <li>“Certificate of Appreciation by Sitara School and Teachers Institute in recognition of outstanding contributions to the academic and spiritual growth and development of Sitara School students for the session 2018-2019.</li>
+            <li> “Certificate of Appreciation by Sitara School and Teachers Institute in recognition of outstanding contributions to the academic and spiritual growth and development of Sitara School students for the session 2019-2020.</li>
           </ul>
         </section>
         
         <section className={styles.section}>
           {/* <h2>OTHER EXPERIENCES:</h2> */}
-          <a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.herokuapp.com?font=Arial&size=30&duration=5004&pause=1000&color=2FF72D&random=false&width=435&lines=OTHER EXPERIENCES" alt="Typing SVG" /></a>
+          <a href="https://git.io/typing-svg"><Image src="https://readme-typing-svg.herokuapp.com?font=Arial&size=30&duration=5004&pause=1000&color=2FF72D&random=false&width=435&lines=OTHER EXPERIENCES" alt="Typing SVG" /></a>
           <ul>
-            <li>Board Member of Sitara School & Teachers’ Institute. (Trust Regd.) from last 9 years.</li>
-            <li>Teachers’ trainer at Sitara School & Teachers’ Institute. (Trust Regd.) from last 5 years.</li>
+            <li>Board Member of Sitara School & Teachers Institute. (Trust Regd.) from last 9 years.</li>
+            <li>Teachers trainer at Sitara School and Teachers Institute. (Trust Regd.) from last 5 years.</li>
             <ul>
               <li>Conceptual Teaching</li>
               <li>Self Development</li>
@@ -206,12 +207,12 @@ export default function Home() {
         
         <section className={styles.section}>
           <h2>INTERESTS:</h2>
-          <p>Book reading & net surfing especially related to “IT,Self Development & Personal Growth”. Loves to spend time in “Taffakur”. Special Interest in Sufism, Islamic History, and Exploring the Quran. Philosophy and psychology are also my favorite subjects. Above all, I love to study humans. In sports, I like to play cricket and badminton.</p>
+          <p>Book reading and net surfing especially related to IT,Self Development and Personal Growth. Loves to spend time in Taffakur. Special Interest in Sufism, Islamic History, and Exploring the Quran. Philosophy and psychology are also my favorite subjects. Above all, I love to study humans. In sports, I like to play cricket and badminton.</p>
         </section>
         
         <section className={styles.section}>
           <h2>LANGUAGES:</h2>
-          <p>Fluent in Punjabi, Urdu & English.</p>
+          <p>Fluent in Punjabi, Urdu and English.</p>
         </section>
         
         <section className={styles.section}>
